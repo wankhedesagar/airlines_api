55ef3379db9b054ded49351cad05594113135d9f
sagar wankhede <swankhede639@gmail.com>
2024-07-12 18:37:37 +0530

    type your api key

---
+++ src/component/Airlines.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+import Sppiner from "./Sppiner";
+
+function Airlines() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    var name = "Air";
+    $.ajax({
+      method: "GET",
+      url: "https://api.api-ninjas.com/v1/airlines?name=" + name,
+      headers: { "X-Api-Key": "XsCX9xpbV0Pk4OusYVzYCA==nG1FSL9mMg7XRr2r" },
+      contentType: "application/json",
+      success: function (result) {
+        setLoading(true);
+        console.log(result);
+        setData(result);
+      },
+      error: function ajaxError(jqXHR) {
+        console.error("Error: ", jqXHR.responseText);
+        setLoading(false);
+      },
+    });
+  }, []);
+
+  return (
+    <React.Fragment>
+    <div className="container">
+      {!loading && <Sppiner/>}
+      
+        {data.map((items, id) => {
+          return (
+            <div key={id} className="card">
+              <div className="card-content">
+                <h5 className="">iata: {items.iata}</h5>
+                <p className="">icao: {items.icao}</p>
+                <p className="">name: {items.name}</p>
+              </div>
+              <div className="logo">
+              <a href={items.logo_url} target="_blank"><img src={items.logo_url} className="" alt="logo" /></a>
+              </div>
+
+            </div>
+          );
+        })}
+      
+    </div>
+    </React.Fragment>
+  );
+}
+
+export default Airlines;
